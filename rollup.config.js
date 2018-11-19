ce2c59cb1c0bf0b42b537d631de6862664b350c7
Robert Gove <rpgove@gmail.com>
2018-11-18 23:16:30 -0500

    Build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,18 @@
 const definition = require("./package.json");
 
-export default {
-  input: "index",
-  output: {
-    extend: true,
-    file: `build/${definition.name}.js`,
-    format: "umd",
-    name: "d3"
+export default [
+  {
+    input: "index",
+    external: Object.keys(definition.dependencies || {}).filter(key => /^d3-/.test(key)),
+    output: {
+      indent: false,
+      extend: true,
+      file: `build/${definition.name}.js`,
+      format: "umd",
+      name: "d3",
+      banner: `// Copyright ${(new Date).getFullYear()} Two Six Labs, LLC. v${definition.version} ${definition.name} ${definition.homepage}`,
+      globals: Object.assign({}, ...Object.keys(definition.dependencies || {}).filter(key => /^d3-/.test(key)).map(key => ({[key]: "d3"})))
+    },
+    plugins: []
   }
-};
+];
